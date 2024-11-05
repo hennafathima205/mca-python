85ce3867f567fe414dd4d59bf1cd898014e948a4
hennafathima205 <hennafathimakodakkatakath@gmail.com>
2024-11-05 12:29:14 +0530

    Create swaping.py

---
+++ week1/swaping.py
@@ -0,0 +1,9 @@
+x=int(input("enter the first number"))
+y=int(input("enter the second number"))
+print("before swaping")
+print("value of x:",x,"and y:",y)
+x=x+y
+y=x-y
+x=x-y
+print("after swaping:")
+print("value of x:",x,"and y:",y)
