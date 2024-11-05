9bef47a5c4904b92903cb737ad9aa1c941873c6f
hennafathima205 <hennafathimakodakkatakath@gmail.com>
2024-11-05 12:30:29 +0530

    Delete swaping.py

--- swaping.py
+++
@@ -1,9 +0,0 @@
-x=int(input("enter the first number"))
-y=int(input("enter the second number"))
-print("before swaping")
-print("value of x:",x,"and y:",y)
-x=x+y
-y=x-y
-x=x-y
-print("after swaping:")
-print("value of x:",x,"and y:",y)
