34d27368ee78f4f1bc4479e3999204a1aa8c877c
hennafathima205 <hennafathimakodakkatakath@gmail.com>
2024-11-05 12:30:17 +0530

    Delete subject.py

--- subject.py
+++
@@ -1,9 +0,0 @@
-s1=int(input("enter the mark of first subject"))
-s2=int(input("enter the mark of second subject"))
-s3=int(input("enter the mark of third subject"))
-s4=int(input("enter the mark of fourth subject"))
-s5=int(input("enter the mark of fifth subject"))
-total=s1+s2+s3+s4+s5
-print(total)
-percentage=(total/500)*100
-print(percentage)
